33b8c4ce12bcf52cfe271dae193b9a6b81a44548
raibot <46849811+raibot007@users.noreply.github.com>
2023-10-31 23:32:34 +0530

    Add files via upload
    Adding POC code to train guided mini GANs

--- GuidedMiniGANs.py
+++ GuidedMiniGANs.py
@@ -281,3 +281,205 @@ D, G1, G2, G3, G4  = build_network(d_conv_dim, g_conv_dim, z_size)
 ND=torch.load('./noisy_discriminator.pt')
 
 
+# Check for a GPU
+train_on_gpu = torch.cuda.is_available()
+if not train_on_gpu:
+    print('No GPU found. Please use a GPU to train your neural network.')
+else:
+    print('Training on GPU!')
+
+
+def real_loss(D_out):
+    '''Calculates how close discriminator outputs are to being real.
+       param, D_out: discriminator logits
+       return: real loss'''
+    batch_size = D_out.size(0)
+    # label smoothing
+    # smooth, real labels = 0.9
+    labels = torch.ones(batch_size)*0.9
+    # move labels to GPU if available
+    if train_on_gpu:
+        labels = labels.cuda()
+    # binary cross entropy with logits loss
+    criterion = nn.BCEWithLogitsLoss()
+    # calculate loss
+    loss = criterion(D_out.squeeze(), labels)
+    return loss
+
+def fake_loss(D_out):
+    '''Calculates how close discriminator outputs are to being fake.
+       param, D_out: discriminator logits
+       return: fake loss'''
+    batch_size = D_out.size(0)
+    labels = torch.zeros(batch_size) # fake labels = 0
+    if train_on_gpu:
+        labels = labels.cuda()
+    criterion = nn.BCEWithLogitsLoss()
+    # calculate loss
+    loss = criterion(D_out.squeeze(), labels)
+    return loss
+    
+
+
+import torch.optim as optim
+
+lr = 0.0002
+beta1=0.5
+beta2=0.999 # default value
+
+# Create optimizers for the discriminator D and generator G
+d_optimizer = optim.Adam(D.parameters(), lr, [beta1, beta2])
+g1_optimizer = optim.Adam(G1.parameters(), lr, [beta1, beta2])
+g2_optimizer = optim.Adam(G2.parameters(), lr, [beta1, beta2])
+g3_optimizer = optim.Adam(G3.parameters(), lr, [beta1, beta2])
+g4_optimizer = optim.Adam(G4.parameters(), lr, [beta1, beta2])
+
+
+def train(D, G1, G2, G3, G4, ND, n_epochs, print_every=50):
+    '''Trains adversarial networks for some number of epochs
+       param, D: the discriminator network
+       param, G: the generator network
+       param, n_epochs: number of epochs to train for
+       param, print_every: when to print and record the models' losses
+       return: D and G losses'''
+
+    # move models to GPU
+    if train_on_gpu:
+        D.cuda()
+        G1.cuda()
+        G2.cuda()
+        G3.cuda()
+        G4.cuda()
+        ND.cuda()
+    
+    ND.eval()
+
+    # keep track of loss and generated, "fake" samples
+    samples = []
+    losses = []
+
+    # Get some fixed data for sampling. These are images that are held
+    # constant throughout training, and allow us to inspect the model's performance
+    sample_size=16
+    fixed_z = np.random.uniform(-1, 1, size=(sample_size, z_size))
+    fixed_z = torch.from_numpy(fixed_z).float()
+    # move z to GPU if available
+    if train_on_gpu:
+        fixed_z = fixed_z.cuda()
+
+    # epoch training loop
+    for epoch in range(n_epochs):
+
+        # batch training loop
+        for batch_i, (real_images, _) in enumerate(clean_images_loader):
+
+            batch_size = real_images.size(0)
+            real_images = scale(real_images)
+
+            # 1. Train the discriminator on real and fake images
+            d_optimizer.zero_grad()
+            if train_on_gpu:
+                real_images = real_images.cuda()
+            D_real = D(real_images)
+            d_real_loss = real_loss(D_real)
+            z = np.random.uniform(-1, 1, size=(batch_size, z_size))
+            z = torch.from_numpy(z).float()
+            if train_on_gpu:
+                z = z.cuda()
+            fake_images = G4(G3(G2(G1(z).view(batch_size,-1)).view(batch_size,-1)).view(batch_size,-1))
+            D_fake = D(fake_images)
+            d_fake_loss = fake_loss(D_fake)
+            d_loss = d_real_loss + d_fake_loss
+            d_loss.backward()
+            d_optimizer.step()
+
+            # 2. Train the generator with an adversarial loss
+            g1_optimizer.zero_grad()
+            g2_optimizer.zero_grad()
+            g3_optimizer.zero_grad()
+            g4_optimizer.zero_grad()
+            z = np.random.uniform(-1, 1, size=(batch_size, z_size))
+            z = torch.from_numpy(z).float()
+            if train_on_gpu:
+                z = z.cuda()
+            G1_images=G1(z)
+            ND1_loss = real_loss(ND(G1_images))
+            G2_images=G2(G1_images.view(batch_size,-1))
+            ND2_loss = real_loss(ND(G2_images))
+            G3_images=G3(G2_images.view(batch_size,-1))
+            ND3_loss = real_loss(ND(G3_images))
+            fake_images = G4(G3_images.view(batch_size,-1))
+            D_fake = D(fake_images)
+            g_loss = real_loss(D_fake)+ND3_loss+ND2_loss+ND1_loss
+            g_loss.backward()
+            g4_optimizer.step()
+            g3_optimizer.step()
+            g2_optimizer.step()
+            g1_optimizer.step()
+
+
+            # Print some loss stats
+            if batch_i % print_every == 0:
+                # append discriminator loss and generator loss
+                losses.append((d_loss.item(), g_loss.item()))
+                # print discriminator and generator loss
+                print('Epoch [{:5d}/{:5d}] | d_loss: {:6.4f} | g_loss: {:6.4f}'.format(
+                        epoch+1, n_epochs, d_loss.item(), g_loss.item()))
+
+
+        ## AFTER EACH EPOCH##
+        # this code assumes your generator is named G, feel free to change the name
+        # generate and save sample, fake images
+        G4.eval() # for generating samples
+        G3.eval()
+        G2.eval()
+        G1.eval()
+        samples_z = G4(G3(G2(G1(fixed_z).view(sample_size,-1)).view(sample_size,-1)).view(sample_size,-1))
+        samples.append(samples_z)
+        G4.train() # back to training mode
+        G3.train()
+        G2.train()
+        G1.train()
+
+    # Save training generator samples
+    with open('train_samples.pkl', 'wb') as f:
+        pkl.dump(samples, f)
+
+    # finally return losses
+    return losses
+    
+# set number of epochs
+n_epochs = 30
+
+
+# call training function
+losses = train(D, G1,G2,G3,G4,ND, n_epochs=n_epochs)
+
+fig, ax = plt.subplots()
+losses = np.array(losses)
+plt.plot(losses.T[0], label='Discriminator', alpha=0.5)
+plt.plot(losses.T[1], label='Generator', alpha=0.5)
+plt.title("Training Losses")
+plt.legend()
+
+
+# helper function for viewing a list of passed in sample images
+def view_samples(epoch, samples):
+    fig, axes = plt.subplots(figsize=(16,4), nrows=2, ncols=8, sharey=True, sharex=True)
+    for ax, img in zip(axes.flatten(), samples[epoch]):
+        img = img.detach().cpu().numpy()
+        img = np.transpose(img, (1, 2, 0))
+        img = ((img + 1)*255 / (2)).astype(np.uint8)
+        ax.xaxis.set_visible(False)
+        ax.yaxis.set_visible(False)
+        im = ax.imshow(img.reshape((64,64,3)))
+
+
+
+# Load samples from generator, taken while training
+with open('train_samples.pkl', 'rb') as f:
+    samples = pkl.load(f)
+
+
+_ = view_samples(-1, samples)
+
